cbcc7b922a5d020b45099bd573c63c982be4ce72
Radison Akerman <rak3rman@gmail.com>
2023-04-15 20:56:10 -0500

    feat: decomp classes page into components

---
+++ nuxt/components/Sidebar/Body/Classes.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <ul role="list" class="-mx-2 space-y-1">
+      <li v-for="item in classes" :key="item.title">
+        <a
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-primary-focus text-neutral'
+              : 'text-accent hover:text-neutral hover:bg-primary-focus',
+            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
+          ]"
+        >
+          <component
+            :is="item.icon"
+            class="h-6 w-6 shrink-0"
+            aria-hidden="true"
+          />
+          {{ item.number }}
+        </a>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  classes: Object,
+});
+</script>
---
+++ nuxt/components/Sidebar/Body/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="flex grow flex-col gap-y-5 overflow-y-auto bg-primary px-6 ring-1 ring-accent/5"
+  >
+    <div class="flex h-16 shrink-0 items-center">
+      <img
+        class="mt-4 h-7 w-auto"
+        src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/7bbf62eb-c79e-41ef-f2fb-caab76dc6700/md"
+        alt="Your Company"
+      />
+    </div>
+    <nav class="flex flex-1 flex-col">
+      <ul role="list" class="flex flex-1 flex-col gap-y-7">
+        <SidebarBodyClasses :classes="classes" />
+        <SidebarBodyUser :userAuthor="userAuthor" />
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  classes: Object,
+  userAuthor: Object,
+});
+</script>
